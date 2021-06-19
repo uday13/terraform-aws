827ca9de148c1e930fafd2fe0dae7f0fb1341760
uday13 <morthala.udaykumar@gmail.com>
2021-06-18 23:28:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,27 @@ resource "aws_instance" "test" {
     private_key = "${file("${var.instance_key_file}")}"
   }
   
+# both resources can be written as part of a module (example name: instance_by_region)
+resource "aws_instance" "instance_us-east-1" {
+  region = var.aws_ec2_region ? 1 : 0
+  
+  subnet_id              = "${data.aws_subnet.az_a.id}"
+  ami                    = "${var.ami_us-east["oel-7.9_2021_06"]}"
+  key_name               = "${var.key_name}"
+  tag_environment        = "${var.tag_environment}"
+  user_data              = "${var.user_data}"
+  instance_key_file      = "${var.instance_key_file}"
+  private_ip             = ""
+}
+  
+resource "aws_instance" "instance_eu-central-1" {
+  region = var.aws_ec2_region ? 1 : 0
+  
+  subnet_id              = "${data.aws_subnet.az_a.id}"
+  ami                    = "${var.ami_eu-central["oel-7.9_2021_06"]}"
+  key_name               = "${var.key_name}"
+  tag_environment        = "${var.tag_environment}"
+  user_data              = "${var.user_data}"
+  instance_key_file      = "${var.instance_key_file}"
+  private_ip             = ""
+}
