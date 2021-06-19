9800c213b82e4293ae3fad0ea2ed6e87014327ab
uday13 <morthala.udaykumar@gmail.com>
2021-06-18 22:51:39 -0500

    Create environmental_vars.tf

---
+++ environmental_vars.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ami_us-east" {
+  type = "map"
+
+  default = {
+    oel-7.9_202110 = "ami-0263b1da39775563a"
+  }
+}
+
+variable "subnet_vpc_mgmt" {
+  type = "map"
+
+  default = {
+    us-east-2a = "subnet-26202d50"
+    us-east-2b = "subnet-6eddc10a"
+  }
+}
+
+variable "tag_environment" {
+  default = "Development"
+}
+
+variable "key_name" {
+  description = "AWS key name to use when creating new EC2 instances"
+}
+
+variable "instance_key_file" {
+  description = "The key file location to use in provisioning operations."
+}
