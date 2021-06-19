0851fd591e80687ca8ccab9fce18a64fc4a8a797
uday13 <morthala.udaykumar@gmail.com>
2021-06-18 23:20:23 -0500

    Update config.tf

--- config.tf
+++ config.tf
@@ -12,7 +12,7 @@ data "aws_subnet" "az_a" {
   id = "${var.subnet_vpc_mgmt["us-east-1a"]}"
 }
 
-
+# 1. creating test instance 
 module "test-instance" {
   source = 
   subnet_id              = "${data.aws_subnet.az_a.id}"
@@ -24,7 +24,8 @@ module "test-instance" {
   instance_key_file      = "${var.instance_key_file}"
   private_ip             = ""
 }
-  
+ 
+# 2. crearting sns and sqs topic and subscribe  
 resource "aws_sns_topic" "test" {
   name = "test-topic"
 }
@@ -38,3 +39,26 @@ resource "aws_sns_topic_subscription" "test_sqs_target" {
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.test_queue.arn
 }
+  
+# 3. create conditional ece instances
+resource "aws_instance" "instance_us-east-1" {
+  count = var.aws_region ? 1 : 0
+  scheduled_action_name  = "scale-out-during-business-hours"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 10
+  recurrence             = "0 9 * * *"
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+  
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name  = "scale-in-at-night"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 2
+  recurrence             = "0 17 * * *"
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+  
+  
