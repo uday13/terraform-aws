7cc5e1a9b1f1f33b7548d7fef954a662a9ddea23
uday13 <morthala.udaykumar@gmail.com>
2021-06-18 22:39:06 -0500

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" {
+  # subnet ID and availability zone for our VPC
+  subnet_id = "${var.subnet_id}"
+
+  instance_type               = "${var.instance_type}"
+  associate_public_ip_address = "false"
+
+  vpc_security_group_ids = [
+    "${var.vpc_security_group_ids}",
+  ]
+
+  # Lookup the correct AMI based on the region we specified
+  ami = "${var.ami}"
+
+  # The name of our SSH keypair you've created and downloaded from the AWS console.
+  key_name = "${var.key_name}"
+
+  private_ip = "${var.private_ip}"
+
+  user_data = "${var.user_data}"
+
+  connection {
+    user        = "${var.user}"
+    private_key = "${file("${var.instance_key_file}")}"
+  }
+  
